8126ee96207f1bf0f399264758f26fd9c7f59f16
FeliGW <alloisfelipe9@gmail.com>
2025-09-19 15:51:14 -0300

    funcion linq 1
    al final el gitignore no anduvo

--- IA2 Parcial 1/Assets/Scrips/Guild.cs
+++ IA2 Parcial 1/Assets/Scrips/Guild.cs
@@ -1,6 +1,8 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using System;
+using System.Linq;
 
 public class Guild : MonoBehaviour
 {
@@ -18,4 +20,48 @@ public class Guild : MonoBehaviour
     {
         
     }
+
+    public IEnumerator<PartyAnalysis> AnalyzePartiesPerformance()
+    {
+        var allParties = Partys?.Where(p => p.PartyComp != null && p.PartyComp.Count > 0).ToList() ?? new List<Party>();
+        
+        for (int i = 0; i < allParties.Count; i += 2)
+        {
+            var currentBatch = allParties.Skip(i).Take(2).ToList();
+            
+            foreach (var party in currentBatch)
+            {
+                var partyCharacters = party.PartyComp.ToList();
+                
+                var orderedCharacters = partyCharacters.OrderByDescending(c => c.Lvl).ToList();
+                
+                var validCharacters = orderedCharacters.Where(c => c.CurrentHP > 0).ToList();
+                
+                var hasInjuredMembers = validCharacters.Any(c => c.CurrentHP < c.MaxHP * 0.5f);
+                
+                var analysis = new PartyAnalysis
+                {
+                    PartyName = party.PartyName,
+                    TotalLevel = validCharacters.Sum(c => c.Lvl),
+                    InjuredCount = validCharacters.Count(c => c.CurrentHP < c.MaxHP * 0.3f),
+                    HasBalanceIssues = hasInjuredMembers,
+                    TopCharacter = orderedCharacters.FirstOrDefault()?.Name ?? "None"
+                };
+
+                yield return analysis;
+            }
+            
+            if (i + 2 < allParties.Count)
+                yield return null;
+        }
+    }
+}
+
+public class PartyAnalysis
+{
+    public string PartyName;
+    public int TotalLevel;
+    public int InjuredCount;
+    public bool HasBalanceIssues;
+    public string TopCharacter;
 }
